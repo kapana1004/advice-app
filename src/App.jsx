e26fed4ce7dda2fa61a883c2237fd6a261f9457d
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2023-11-22 14:50:06 +0400

    add windov reload function for update advice

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import axios from "axios";
 function App() {
   const [advice, setAdvice] = useState("");
 
+  const reloadButton = () => {
+    window.location.reload();
+  };
+
   useEffect(() => {
     const requestAdvice = async () => {
       const response = await axios.get("https://api.adviceslip.com/advice");
@@ -20,7 +24,13 @@ function App() {
   return (
     <div>
       <h1 className=" text-lime-500">Hello World</h1>
-      <p className=""> {advice} </p>
+      <p className=""> {advice}</p>
+      <button
+        onClick={reloadButton}
+        className=" w-[100px] h-[30px] rounded cursor-pointer bg-lime-600"
+      >
+        give advice
+      </button>
     </div>
   );
 }
